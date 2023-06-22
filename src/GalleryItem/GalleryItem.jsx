a7556317a0ca083490efd76c9ca486b2dd3148e8
Daniel Davidson <danielmcraedavidson@gmail.com>
2023-06-22 16:57:34 -0500

    added toggle button

--- src/GalleryItem/GalleryItem.jsx
+++ src/GalleryItem/GalleryItem.jsx
@@ -6,15 +6,19 @@ export function GalleryItem ({id, imagePath, info, like}) {
     const [toggle, setToggle] = useState(false);
 
     const toggleImage = () => {
-        // switch toggle from false to true and vice versa
+    // switch toggle from false to true and vice versa
         setToggle(!toggle);
     };
 
     return (
         <div className="gallery-image"> 
-            {`${toggle}`}
-            <img key={id} src={imagePath} onClick={toggleImage}/>
-            {like}
+            {/* {`${toggle}`} */}
+
+            {toggle 
+            ? <p onClick={toggleImage} style={{cursor: "pointer"}}>{info} </p> 
+            : <img key={id} src={imagePath} onClick={toggleImage} style={{cursor: "pointer"}}/>
+            }
+            <br/>
             <button onClick={() => {setLikeCount(likeCount +1)}}>
                 ♥️ Likes {likeCount} 
             </button>
